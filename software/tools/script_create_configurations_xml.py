35b9f8761682b0df723ca3f40d6b5cea3a1d4698
Hongquan Li <hqlisu@gmail.com>
2020-10-04 21:11:50 -0700

    add a script for creating the configurations xml file

---
+++ software/tools/script_create_configurations_xml.py
@@ -0,0 +1,48 @@
+from lxml import etree as ET
+top = ET.Element('modes')
+
+mode_1 = ET.SubElement(top,'mode')
+# ID = ET.SubElement(mode_1,'ID')
+# ID.text = '123'
+mode_1.set('ID','1')
+mode_1.set('Name','BF_1')
+mode_1.set('ExposureTime','100')
+mode_1.set('AnalogGain','10')
+mode_1.set('IlluminationSource','1')
+mode_1.set('CameraSN','')
+
+mode_2 = ET.SubElement(top,'mode')
+mode_2.set('ID','2')
+mode_2.set('Name','BF_2')
+mode_2.set('ExposureTime','100')
+mode_2.set('AnalogGain','10')
+mode_2.set('IlluminationSource','2')
+mode_2.set('CameraSN','')
+
+mode_3 = ET.SubElement(top,'mode')
+mode_3.set('ID','3')
+mode_3.set('Name','Fluorescence 405 nm Ex')
+mode_3.set('ExposureTime','100')
+mode_3.set('AnalogGain','10')
+mode_3.set('IlluminationSource','11')
+mode_3.set('CameraSN','')
+
+mode_4 = ET.SubElement(top,'mode')
+mode_4.set('ID','4')
+mode_4.set('Name','Fluorescence 488 nm Ex')
+mode_4.set('ExposureTime','100')
+mode_4.set('AnalogGain','10')
+mode_4.set('IlluminationSource','12')
+mode_4.set('CameraSN','')
+
+mode_5 = ET.SubElement(top,'mode')
+mode_5.set('ID','5')
+mode_5.set('Name','Fluorescence 638 nm Ex')
+mode_5.set('ExposureTime','100')
+mode_5.set('AnalogGain','10')
+mode_5.set('IlluminationSource','13')
+mode_5.set('CameraSN','')
+
+# print(ET.tostring(top, encoding="UTF-8", pretty_print=True).decode())
+tree = ET.ElementTree(top)
+tree.write('configurations.xml',encoding="utf-8", xml_declaration=True, pretty_print=True)
