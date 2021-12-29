6c3522d854e92bf7d8a1eea03b751b0ca3aa9955
Hongquan Li <hqlisu@gmail.com>
2021-12-29 15:07:05 -0800

    rearrange the GUI display

--- software/control/gui.py
+++ software/control/gui.py
@@ -15,6 +15,9 @@ import control.core as core
 import control.microcontroller as microcontroller
 from control._def import *
 
+import pyqtgraph.dockarea as dock
+SINGLE_WINDOW = True # set to False if use separate windows for display and control
+
 class OctopiGUI(QMainWindow):
 
 	# variables
@@ -30,8 +33,13 @@ class OctopiGUI(QMainWindow):
 		else:
 			self.imageDisplayWindow = core.ImageDisplayWindow(draw_crosshairs=True)
 		self.imageArrayDisplayWindow = core.ImageArrayDisplayWindow() 
-		self.imageDisplayWindow.show()
-		self.imageArrayDisplayWindow.show()
+		# self.imageDisplayWindow.show()
+		# self.imageArrayDisplayWindow.show()
+
+		# image display windows
+		self.imageDisplayTabs = QTabWidget()
+		self.imageDisplayTabs.addTab(self.imageDisplayWindow.widget, "Live View")
+		self.imageDisplayTabs.addTab(self.imageArrayDisplayWindow.widget, "Multichannel Acquisition")
 
 		# load objects
 		if is_simulation:
@@ -79,19 +87,53 @@ class OctopiGUI(QMainWindow):
 		self.recordTabWidget.addTab(self.multiPointWidget, "Multipoint Acquisition")
 
 		# layout widgets
-		layout = QGridLayout() #layout = QStackedLayout()
-		layout.addWidget(self.cameraSettingWidget,0,0)
-		layout.addWidget(self.liveControlWidget,1,0)
-		layout.addWidget(self.navigationWidget,2,0)
+		layout = QVBoxLayout() #layout = QStackedLayout()
+		layout.addWidget(self.cameraSettingWidget)
+		layout.addWidget(self.liveControlWidget)
+		layout.addWidget(self.navigationWidget)
 		if SHOW_DAC_CONTROL:
-			layout.addWidget(self.dacControlWidget,3,0)
-		layout.addWidget(self.autofocusWidget,4,0)
-		layout.addWidget(self.recordTabWidget,5,0)
+			layout.addWidget(self.dacControlWidget)
+		layout.addWidget(self.autofocusWidget)
+		layout.addWidget(self.recordTabWidget)
+		layout.addStretch()
 		
 		# transfer the layout to the central widget
 		self.centralWidget = QWidget()
 		self.centralWidget.setLayout(layout)
-		self.setCentralWidget(self.centralWidget)
+		# self.centralWidget.setFixedSize(self.centralWidget.minimumSize())
+		# self.centralWidget.setFixedWidth(self.centralWidget.minimumWidth())
+		# self.centralWidget.setMaximumWidth(self.centralWidget.minimumWidth())
+		self.centralWidget.setFixedWidth(self.centralWidget.minimumSizeHint().width())
+		
+		if SINGLE_WINDOW:
+			dock_display = dock.Dock('Image Display', autoOrientation = False)
+			dock_display.showTitleBar()
+			dock_display.addWidget(self.imageDisplayTabs)
+			dock_display.setStretch(x=100,y=None)
+			dock_controlPanel = dock.Dock('Controls', autoOrientation = False)
+			# dock_controlPanel.showTitleBar()
+			dock_controlPanel.addWidget(self.centralWidget)
+			dock_controlPanel.setStretch(x=1,y=None)
+			dock_controlPanel.setFixedWidth(dock_controlPanel.minimumSizeHint().width())
+			main_dockArea = dock.DockArea()
+			main_dockArea.addDock(dock_display)
+			main_dockArea.addDock(dock_controlPanel,'right')
+			self.setCentralWidget(main_dockArea)
+			desktopWidget = QDesktopWidget()
+			height_min = 0.96*desktopWidget.height()
+			width_min = 0.96*desktopWidget.width()
+			self.setMinimumSize(width_min,height_min)
+		else:
+			self.setCentralWidget(self.centralWidget)
+			self.tabbedImageDisplayWindow = QMainWindow()
+			self.tabbedImageDisplayWindow.setCentralWidget(self.imageDisplayTabs)
+			self.tabbedImageDisplayWindow.setWindowFlags(self.windowFlags() | Qt.CustomizeWindowHint)
+			self.tabbedImageDisplayWindow.setWindowFlags(self.windowFlags() & ~Qt.WindowCloseButtonHint)
+			desktopWidget = QDesktopWidget()
+			width = 0.96*desktopWidget.height()
+			height = width
+			self.tabbedImageDisplayWindow.setFixedSize(width,height)
+			self.tabbedImageDisplayWindow.show()
 
 		# make connections
 		self.streamHandler.signal_new_frame_received.connect(self.liveController.on_new_frame)
@@ -105,7 +147,7 @@ class OctopiGUI(QMainWindow):
 		if ENABLE_TRACKING:
 			self.navigationController.signal_joystick_button_pressed.connect(self.trackingControlWidget.slot_joystick_button_pressed)
 		self.autofocusController.image_to_display.connect(self.imageDisplayWindow.display_image)
-		# self.multipointController.image_to_display.connect(self.imageDisplayWindow.display_image)
+		self.multipointController.image_to_display.connect(self.imageDisplayWindow.display_image)
 		self.multipointController.signal_current_configuration.connect(self.liveControlWidget.set_microscope_mode)
 		self.multipointController.image_to_display_multi.connect(self.imageArrayDisplayWindow.display_image)
 		self.liveControlWidget.signal_newExposureTime.connect(self.cameraSettingWidget.set_exposure_time)
@@ -120,6 +162,8 @@ class OctopiGUI(QMainWindow):
 		self.camera.close()
 		self.imageSaver.close()
 		self.imageDisplay.close()
-		self.imageDisplayWindow.close()
-		self.imageArrayDisplayWindow.close()
+		if not SINGLE_WINDOW:
+			self.imageDisplayWindow.close()
+			self.imageArrayDisplayWindow.close()
+			self.tabbedImageDisplayWindow.close()
 		self.microcontroller.close()
