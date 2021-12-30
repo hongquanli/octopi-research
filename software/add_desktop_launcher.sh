2debeab706e533f0d08c7bdc16c79e5a25473456
Hongquan Li <hqlisu@gmail.com>
2021-12-30 15:07:43 -0800

    add desktop launcher

---
+++ software/add_desktop_launcher.sh
@@ -0,0 +1,3 @@
+cp octopi-research.desktop ~/.local/share/applications/
+chmod u+x ~/.local/share/applications/octopi-research.desktop
+cp ~/.local/share/applications/octopi-research.desktop ~/desktop/
