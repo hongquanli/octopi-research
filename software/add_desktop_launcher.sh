684a5524154042f1bf202aaa21d7f531f739bf11
Hongquan Li <hqlisu@gmail.com>
2021-12-30 15:10:37 -0800

    typo fix

--- software/add_desktop_launcher.sh
+++ software/add_desktop_launcher.sh
@@ -1,3 +1,3 @@
 cp octopi-research.desktop ~/.local/share/applications/
 chmod u+x ~/.local/share/applications/octopi-research.desktop
-cp ~/.local/share/applications/octopi-research.desktop ~/desktop/
+cp ~/.local/share/applications/octopi-research.desktop ~/Desktop/
