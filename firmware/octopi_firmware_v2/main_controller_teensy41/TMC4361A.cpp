b96b63082c620a7d28b43405aaf3eec76df7b7a9
Hongquan Li <hqlisu@gmail.com>
2022-08-09 17:51:46 -0700

    set actuator default range to -inf to +inf

--- firmware/octopi_firmware_v2/main_controller_teensy41/TMC4361A.cpp
+++ firmware/octopi_firmware_v2/main_controller_teensy41/TMC4361A.cpp
@@ -145,8 +145,8 @@ void tmc4361A_init(TMC4361ATypeDef *tmc4361A, uint8_t channel, ConfigurationType
   tmc4361A->velocity  = 0;
   tmc4361A->oldTick   = 0;
   tmc4361A->oldX      = 0;
-  tmc4361A->xmin      = 0;
-  tmc4361A->xmax      = 0;
+  tmc4361A->xmin      = -2147483648;
+  tmc4361A->xmax      = 2147483647;
   tmc4361A->xhome     = 0;
   tmc4361A->config    = config;
 
