5310994bf1a42b624005adad00b05048071f9117
Ali Cataltepe <ali@cataltepe.com>
2023-11-15 10:41:09 -0800

    tweaked desktop shortcut creation script

--- software/tools/script_create_desktop_shortcut.py
+++ software/tools/script_create_desktop_shortcut.py
@@ -1,7 +1,48 @@
 import os
 import stat
+def create_desktop_shortcut_simulation(directory_path, script_name):
+    squid_suffix = script_name.replace("main_","")
+    if squid_suffix != "main" and squid_suffix != "":
+        shortcut_content = f'''\
+[Desktop Entry]
+Name=Squid_{squid_suffix}_simulation
+Exec=gnome-terminal --working-directory="{directory_path}" -e "/usr/bin/env python3 {directory_path}/{script_name}.py --simulation"
+Type=Application
+Terminal=true
+'''
+    else:
+         shortcut_content = f'''\
+[Desktop Entry]
+Name=Squid_simulation
+Exec=gnome-terminal --working-directory="{directory_path}" -e "/usr/bin/env python3 {directory_path}/{script_name}.py --simulation"
+Type=Application
+Terminal=true
+'''
+
+    if squid_suffix != "main" and squid_suffix != "":
+        desktop_path_base = f'~/Desktop/Squid_{squid_suffix}_simulation.desktop'
+    else:
+        desktop_path_base = f'~/Desktop/Squid_simulation.desktop'
+    desktop_path = os.path.expanduser(desktop_path_base)
+    with open(desktop_path, 'w') as shortcut_file:
+        shortcut_file.write(shortcut_content)
+    os.chmod(desktop_path, stat.S_IRWXU)
+    return desktop_path
+
+
+
 def create_desktop_shortcut(directory_path, script_name):
-    shortcut_content = f'''\
+    squid_suffix = script_name.replace("main_","")
+    if squid_suffix != "main" and squid_suffix != "":
+        shortcut_content = f'''\
+[Desktop Entry]
+Name=Squid_{squid_suffix}
+Exec=gnome-terminal --working-directory="{directory_path}" -e "/usr/bin/env python3 {directory_path}/{script_name}.py"
+Type=Application
+Terminal=true
+'''
+    else:
+         shortcut_content = f'''\
 [Desktop Entry]
 Name=Squid
 Exec=gnome-terminal --working-directory="{directory_path}" -e "/usr/bin/env python3 {directory_path}/{script_name}.py"
@@ -9,7 +50,11 @@ Type=Application
 Terminal=true
 '''
 
-    desktop_path = os.path.expanduser('~/Desktop/Squid.desktop')
+    if squid_suffix != "main" and squid_suffix != "":
+        desktop_path_base = f'~/Desktop/Squid_{squid_suffix}.desktop'
+    else:
+        desktop_path_base = f'~/Desktop/Squid.desktop'
+    desktop_path = os.path.expanduser(desktop_path_base)
     with open(desktop_path, 'w') as shortcut_file:
         shortcut_file.write(shortcut_content)
     os.chmod(desktop_path, stat.S_IRWXU)
@@ -17,11 +62,20 @@ Terminal=true
 
 def main():
     # Prompt for directory path and script name
-    directory_path = input('Enter the directory path (default: current directory): ') or os.getcwd()
-    script_name = input('Enter the script name (without .py extension): ')
+    directory_path = input('Enter the directory path to octopi-research/software (default: current directory): ') or os.getcwd()
+    script_name = input('Enter the main script name under octopi-research/software (without .py extension): ')
+
+    simulation = input('Is this for launching in simulation mode? [NO/yes]: ') or False
+    if str(simulation).lower() == 'yes':
+        simulation = True
+    else:
+        simulation = False
 
     # Create desktop shortcut
-    desktop_path = create_desktop_shortcut(directory_path, script_name)
+    if not simulation:
+        desktop_path = create_desktop_shortcut(directory_path, script_name)
+    else:
+        desktop_path = create_desktop_shortcut_simulation(directory_path, script_name)
     print(f'Desktop shortcut created at: {desktop_path}')
 
 if __name__ == '__main__':
