fbefd1a199ef0c585881051bdb2aabede6c53390
hongquanli <hongquanli@users.noreply.github.com>
2024-01-14 22:05:40 -0500

    Update install_spinnaker.sh

--- software/drivers and libraries/flir/install_spinnaker.sh
+++ software/drivers and libraries/flir/install_spinnaker.sh
@@ -5,5 +5,6 @@ sudo apt-get install libavcodec58 libavformat58 \
 libswscale5 libswresample3 libavutil56 libusb-1.0-0 \
 libpcre2-16-0 libdouble-conversion3 libxcb-xinput0 \
 libxcb-xinerama0
-sudo sh spinnaker-3.1.0.79-amd64/install_spinnaker.sh
-sudo sh spinnaker-3.1.0.79-amd64/configure_usbfs.sh
+cd spinnaker-3.1.0.79-amd64
+sudo sh install_spinnaker.sh
+sudo sh configure_usbfs.sh
