82b40a05490594fb146fbc8207ed3d46d366026d
kevin.wang <kevin.wang2004@hotmail.com>
2024-03-21 15:00:15 +0800

    [win compatible] modify for Daheng camera driver

--- software/control/gxipy/dxwrapper.py
+++ software/control/gxipy/dxwrapper.py
@@ -13,7 +13,10 @@ if sys.platform == 'linux2' or sys.platform == 'linux':
         print('Cannot find libgxiapi.so.')
 else:
     try:
-        dll = WinDLL('DxImageProc.dll')
+        if (sys.version_info.major == 3 and sys.version_info.minor >= 8) or (sys.version_info.major > 3):
+            dll = WinDLL('DxImageProc.dll', winmode=0)
+        else:
+            dll = WinDLL('DxImageProc.dll')
     except OSError:
         print('Cannot find DxImageProc.dll.')
 
