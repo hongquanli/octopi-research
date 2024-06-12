067cfddba7bc77e614549b5cdaedbac309f36809
kevin.wang <kevin.wang2004@hotmail.com>
2024-06-12 17:37:38 +0800

    bug fix for zaber device

--- software/control/_def.py
+++ software/control/_def.py
@@ -578,7 +578,6 @@ USE_ZABER_EMISSION_FILTER_WHEEL = False
 # need redefine it with real USB device serial number
 FILTER_CONTROLLER_SERIAL_NUMBER = "A10NFZP8"
 
-
 ##########################################################
 #### start of loading machine specific configurations ####
 ##########################################################
