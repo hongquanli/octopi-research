a42af58f5b66be63107381cbf15af78a913c9f4b
Soham Mukherjee <soham1202@berkeley.edu>
2024-06-14 14:16:13 -0700

    revert cleanup

--- software/control/camera_toupcam.py
+++ software/control/camera_toupcam.py
@@ -115,7 +115,7 @@ class Camera(object):
         self.GAIN_MAX = 40
         self.GAIN_MIN = 0
         self.GAIN_STEP = 1
-        self.EXPOSURE_TIME_MS_MIN = 0.01
+        self.EXPOSURE_TIME_MS_MIN = 0.1
         self.EXPOSURE_TIME_MS_MAX = 3600000
 
         self.ROI_offset_x = CAMERA_CONFIG.ROI_OFFSET_X_DEFAULT
@@ -344,16 +344,12 @@ class Camera(object):
         self._toupcam_pullmode_started = False
 
     def set_pixel_format(self,pixel_format):
-
         was_streaming = False
         if self.is_streaming:
             was_streaming = True
             self.stop_streaming()
 
         self.pixel_format = pixel_format
-
-        if self._toupcam_pullmode_started:
-            self.camera.Stop()
 
         if self.data_format == 'RAW':
             if pixel_format == 'MONO8':
@@ -750,7 +746,7 @@ class Camera_Simulation(object):
         self.GAIN_MAX = 40
         self.GAIN_MIN = 0
         self.GAIN_STEP = 1
-        self.EXPOSURE_TIME_MS_MIN = 0.01
+        self.EXPOSURE_TIME_MS_MIN = 0.1
         self.EXPOSURE_TIME_MS_MAX = 3600000
 
         self.trigger_mode = None
