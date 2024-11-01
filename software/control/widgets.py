062dba4c991b09a2e669cad1adcdbed9f581bf4a
kevin.wang <kevin.wang2004@hotmail.com>
2024-11-01 13:52:02 +0800

    black level setting function modification

--- software/control/widgets.py
+++ software/control/widgets.py
@@ -775,26 +775,15 @@ class CameraSettingsWidget(QFrame):
         blacklevel_line = QHBoxLayout()
         blacklevel_line.addWidget(QLabel('Black Level'))
 
-        # slider_blackLevel Scaling
-        self.slider_blackLevelScaling = QSlider(Qt.Horizontal)
-        self.slider_blackLevelScaling.setRange(0, 31)
-        self.slider_blackLevelScaling.setValue(int(0))
-        self.slider_blackLevelScaling.setTickPosition(QSlider.TicksBelow)
-        self.slider_blackLevelScaling.setTickInterval(1)
-        self.slider_blackLevelScaling.valueChanged.connect(self.update_blacklevel_scaling)
-        self.slider_blackLevelScaling.setSingleStep(1)
+        self.label_blackLevel = QSpinBox()
+        self.label_blackLevel.setMinimum(0)
+        self.label_blackLevel.setMaximum(31)
+        self.label_blackLevel.valueChanged.connect(self.update_blacklevel_scaling)
+        self.label_blackLevel.setSuffix(" ")
 
-        self.label_blackLevelScaling = QSpinBox()
-        self.label_blackLevelScaling.setMinimum(0)
-        self.label_blackLevelScaling.setMaximum(31)
-        self.label_blackLevelScaling.setValue(self.slider_blackLevelScaling.value())
-        self.label_blackLevelScaling.setSuffix(" ")
+        self.label_blackLevel.valueChanged.connect(lambda v: self.label_blackLevel.setValue(round(v)))
 
-        self.slider_blackLevelScaling.valueChanged.connect(lambda v: self.label_blackLevelScaling.setValue(round(v)))
-        self.label_blackLevelScaling.valueChanged.connect(lambda v: self.slider_blackLevelScaling.setValue(round(v)))
-
-        blacklevel_line.addWidget(self.slider_blackLevelScaling)
-        blacklevel_line.addWidget(self.label_blackLevelScaling)
+        blacklevel_line.addWidget(self.label_blackLevel)
 
         self.camera_layout.addLayout(blacklevel_line)
 
@@ -893,6 +882,7 @@ class CameraSettingsWidget(QFrame):
         self.entry_ROI_width.blockSignals(False)
 
     def update_blacklevel_scaling(self, blacklevel):
+        print(blacklevel)
         try:
             self.camera.set_blacklevel(blacklevel)
         except AttributeError:
