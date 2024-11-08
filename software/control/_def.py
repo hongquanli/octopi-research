ffbba52a0c9abcf8443850ab996618a32f3f0dac
Soham Mukherjee <soham1202@berkeley.edu>
2024-11-08 15:28:50 -0800

    cache sample and objective formats

--- software/control/_def.py
+++ software/control/_def.py
@@ -362,7 +362,6 @@ CAMERA_PIXEL_SIZE_UM = {'IMX290':2.9,'IMX178':2.4,'IMX226':1.85,'IMX250':3.45,'I
 
 TUBE_LENS_MM = 50
 CAMERA_SENSOR = 'IMX226'
-DEFAULT_OBJECTIVE = '10x (Mitutoyo)'
 TRACKERS = ['csrt', 'kcf', 'mil', 'tld', 'medianflow','mosse','daSiamRPN']
 DEFAULT_TRACKER = 'csrt'
 
@@ -445,8 +444,6 @@ Z_STACKING_CONFIG_MAP = {
     2: 'FROM TOP'
 }
 
-# plate format
-WELLPLATE_FORMAT = 384
 DEFAULT_Z_POS_MM = 2
 
 WELLPLATE_OFFSET_X_mm = 0 # x offset adjustment for using different plates
@@ -636,6 +633,18 @@ SAMPLE_FORMATS_CSV_PATH = 'sample_formats.csv'
 
 OBJECTIVES = read_objectives_csv(os.path.join('configurations', OBJECTIVES_CSV_PATH))
 WELLPLATE_FORMAT_SETTINGS = read_sample_formats_csv(os.path.join('configurations', SAMPLE_FORMATS_CSV_PATH))
+
+# Add this after reading the CSV files but before using DEFAULT_OBJECTIVE and WELLPLATE_FORMAT
+try:
+    with open("cache/objective_and_sample_format.txt", 'r') as f:
+        cached_settings = json.load(f)
+        if cached_settings.get('objective') in OBJECTIVES:
+            DEFAULT_OBJECTIVE = cached_settings['objective']
+        if cached_settings.get('wellplate_format') in WELLPLATE_FORMAT_SETTINGS or cached_settings.get('wellplate_format') == 0:
+            WELLPLATE_FORMAT = cached_settings['wellplate_format']
+except (FileNotFoundError, json.JSONDecodeError):
+    DEFAULT_OBJECTIVE = '10x (Mitutoyo)'
+    WELLPLATE_FORMAT = 384
 
 NUMBER_OF_SKIP = WELLPLATE_FORMAT_SETTINGS[WELLPLATE_FORMAT]['number_of_skip'] # num rows/cols to skip on wellplate edge
 WELL_SIZE_MM = WELLPLATE_FORMAT_SETTINGS[WELLPLATE_FORMAT]['well_size_mm']
--- software/control/widgets.py
+++ software/control/widgets.py
@@ -7781,15 +7781,32 @@ class LedMatrixSettingsDialog(QDialog):
 
 
 class SampleSettingsWidget(QFrame):
-    def __init__(self, ObjectivesWidget, WellplateFormatWidget ,*args, **kwargs):
+    def __init__(self, ObjectivesWidget, WellplateFormatWidget, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.objectivesWidget = ObjectivesWidget
         self.wellplateFormatWidget = WellplateFormatWidget
+
+        # Set up the layout
         top_row_layout = QGridLayout()
         top_row_layout.setSpacing(2)
         top_row_layout.setContentsMargins(0, 2, 0, 2)
-        top_row_layout.addWidget(self.objectivesWidget,0,0)
-        top_row_layout.addWidget(self.wellplateFormatWidget,0,1)
-        self.setLayout(top_row_layout)  # Set the layout on the frame
+        top_row_layout.addWidget(self.objectivesWidget, 0, 0)
+        top_row_layout.addWidget(self.wellplateFormatWidget, 0, 1)
+        self.setLayout(top_row_layout)
         self.setFrameStyle(QFrame.Panel | QFrame.Raised)
+
+        # Connect signals for saving settings
+        self.objectivesWidget.signal_objective_changed.connect(self.save_settings)
+        self.wellplateFormatWidget.signalWellplateSettings.connect(lambda *args: self.save_settings())
+
+    def save_settings(self):
+        """Save current objective and wellplate format to cache"""
+        os.makedirs('cache', exist_ok=True)
+        data = {
+            'objective': self.objectivesWidget.dropdown.currentText(),
+            'wellplate_format': self.wellplateFormatWidget.wellplate_format
+        }
+
+        with open('cache/objective_and_sample_format.txt', 'w') as f:
+            json.dump(data, f)
 
