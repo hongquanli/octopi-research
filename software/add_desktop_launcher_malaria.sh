c5d93dbd9e3d16ef74674aadb860fcaeaed98527
Ian O'Hara <ianohara@gmail.com>
2024-12-05 10:03:43 -0800

    setup and cleanup: fix setup scripts for new repo, delete outdate and move tools. Add action for install + tests. (#31)
    This fixes the setup scripts to use the new main repository at
    https://github.com/Cephla-Lab/Squid . It also deletes the outdated
    `setup_octopi.sh` script (Hongquan mentioned it was outdated and no
    longer used), moves some tooling scripts into the `tools` subdirectory,
    and generally does a bit of cleanup.
    
    This also introduces a github workflow that will run our setup scripts
    and run the python tests whenever pull requests are made, or master is
    updated.
    
    You can add new tests in `software/tests` by following the example
    already there.
    
    Tested by: The workflow runs and succeeds.

--- software/add_desktop_launcher_malaria.sh
+++
@@ -1,3 +0,0 @@
-cp octopi-research-malaria.desktop ~/.local/share/applications/
-chmod u+x ~/.local/share/applications/octopi-research-malaria.desktop
-cp ~/.local/share/applications/octopi-research-malaria.desktop ~/Desktop/
--- software/setup_22.04.sh
+++ software/setup_22.04.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
+set -eo pipefail
 
+if [[ -n "$TRACE" ]]; then
+  echo "TRACE variable non-empty, turning on script tracing."
+  set -x
+fi
+
+readonly SRC_ROOT="$HOME/Desktop"
+mkdir -p "$SRC_ROOT"
+readonly SQUID_REPO_HTTP="https://github.com/Cephla-Lab/Squid.git"
+readonly SQUID_REPO_NAME="Squid"
+readonly SQUID_SOFTWARE_ROOT="$SRC_ROOT/$SQUID_REPO_NAME/software"
+readonly DAHENG_CAMERA_DRIVER_ROOT="$SQUID_SOFTWARE_ROOT/drivers and libraries/daheng camera/Galaxy_Linux-x86_Gige-U3_32bits-64bits_1.2.1911.9122"
+readonly DAHENG_CAMERA_DRIVER_API_ROOT="$SQUID_SOFTWARE_ROOT/drivers and libraries/daheng camera/Galaxy_Linux_Python_1.0.1905.9081/api"
+readonly TOUPCAM_UDEV_RULE_PATH="$SQUID_SOFTWARE_ROOT/drivers and libraries/toupcam/linux/udev/99-toupcam.rules"
 # update
 sudo apt update
 
@@ -10,24 +24,24 @@ sudo apt install python3-pyqt5.qtsvg
 
 # clone the repo
 sudo apt-get install git -y
-cd ~/Desktop
-git clone https://github.com/hongquanli/octopi-research.git
-cd octopi-research/software
+cd "$SRC_ROOT"
+git clone "$SQUID_REPO_HTTP" "$SQUID_REPO_NAME"
+cd "$SQUID_SOFTWARE_ROOT"
 mkdir cache
 
 # install libraries 
 pip3 install qtpy pyserial pandas imageio crc==1.3.0 lxml numpy tifffile scipy napari
 pip3 install opencv-python-headless opencv-contrib-python-headless
-pip3 install napari[all] scikit-image dask_image ome_zarr aicsimageio basicpy
+pip3 install napari[all] scikit-image dask_image ome_zarr aicsimageio basicpy pytest
 
 # install camera drivers
-cd ~/Desktop/octopi-research/software/drivers\ and\ libraries/daheng\ camera/Galaxy_Linux-x86_Gige-U3_32bits-64bits_1.2.1911.9122
+cd "$DAHENG_CAMERA_DRIVER_ROOT"
 ./Galaxy_camera.run
-cd ~/Desktop/octopi-research/software/drivers\ and\ libraries/daheng\ camera/Galaxy_Linux_Python_1.0.1905.9081/api
+cd "$DAHENG_CAMERA_DRIVER_API_ROOT"
 python3 setup.py build
 sudo python3 setup.py install
-cd ~/Desktop/octopi-research/software
-sudo cp drivers\ and\ libraries/toupcam/linux/udev/99-toupcam.rules /etc/udev/rules.d
+cd "$SQUID_SOFTWARE_ROOT"
+sudo cp "$TOUPCAM_UDEV_RULE_PATH" /etc/udev/rules.d
 
 # enable access to serial ports without sudo
 sudo usermod -aG dialout $USER
